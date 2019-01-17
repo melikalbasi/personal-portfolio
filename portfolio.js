5be7dbf332b43d08dd4dfa6b0fab9ab7f41249a9
Melika Kalbasi <melikalbasi@gmail.com>
2019-01-16 22:10:25 -0800

    update homepage

--- portfolio.js
+++ portfolio.js
@@ -4,3 +4,6 @@ $("#logo").attr("class", "magictime puffIn justify-content-center text-center m-
 
 
 $(".port").attr("class", "magictime puffIn justify-content-center text-center m-0 ml-5 pt-0");
+
+
+$("h5").attr("class", "magictime puffIn justify-content-center text-center m-0 ml-5 pt-0");
